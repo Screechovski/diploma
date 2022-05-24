d121648c99be633d00cb296b3ef0a64765d627b3
Yaroslav <yaroslav_dmitriev@guarantpay.com>
2022-05-24 23:16:49 +0300

    clean for production

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-import "reset.css";
-import { createApp, h } from "vue";
-import store from "@/store";
-import App from "@/app.vue";
-
-createApp({
-    render: () => h(App),
-}).use(store).mount("#main")
--- src/svg/s-square-filled.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <svg class="svg" x="0px" y="0px" viewBox="0 0 455 455" style="enable-background:new 0 0 455 455;">
-        <rect width="455" height="455"/>
-    </svg>
-</template>
