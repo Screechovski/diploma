9b0956a5780d9b5afeae75aa6edb8add471dc3ce
Yaroslav <yaroslav_dmitriev@guarantpay.com>
2022-06-02 16:09:22 +0300

    clean for prod

--- src/store/popups/popupsState.js
+++
@@ -1,8 +0,0 @@
-export const popupsState = () => ({
-    coordinateAxes: false,
-    rectangleParams: false,
-    axesHelperPostion: false,
-    sphereParams: false,
-    cylinderParams: false,
-    exportModal: false,
-})
